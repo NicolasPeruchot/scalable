7d52f7ceae8624d8e3e426fcfa7e0e665f3a8ead
NicolasPeruchot <nicolas.peruchot@gmail.com>
2021-11-16 21:55:59 +0100

    Plot

--- steam_challenge/plot.py
+++ steam_challenge/plot.py
@@ -8,3 +8,15 @@ def plot_reviews(data: pd.DataFrame, feature: str, hue: str):
     """Plot with seaborn."""
     sns.displot(data, x=feature, hue=hue, stat="density", common_norm=False)
     return None
+
+
+def get_dup(data: pd.DataFrame):
+    """Frequence od duplicated rows."""
+    dup = round(data.duplicated().sum() / len(data), 2)
+    return f"Frequence of duplicated rows: \n{dup}"
+
+
+def get_missing(data: pd.DataFrame):
+    """Frequence of missing values."""
+    mis = data.isna().sum() / len(data)
+    return f"Frequence of missing values in each columns:\n {mis}"
